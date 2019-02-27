eb0c5c1be8bb7d84328829538d8ec41dfc6316bb
Akshay Bhatia <akksbhaia@gmail.com>
2019-02-27 12:10:26 +0530

    cms_init

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //reset users table
+        DB::table('users')->truncate();
+
+        //create fake users
+        DB::table('users')->insert([
+           [
+               'name' => "Akshay Bhatia",
+               'email' => "akksbhatia@gmail.com",
+               'password' => bcrypt('secret')
+           ],
+            [
+                'name' => "Amit",
+                'email' => "amit@gmail.com",
+                'password' => bcrypt('secret')
+            ],
+            [
+                'name' => "Akash",
+                'email' => "akash@gmail.com",
+                'password' => bcrypt('secret')
+            ],
+
+
+
+        ]);
+
+    }
+}
